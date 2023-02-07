a7f3fafd8f15c38280fb3abca5b72ae703fb9824
b3rkbal <120989658+b3rkbal@users.noreply.github.com>
2023-02-07 18:36:40 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,1 +1,1 @@
-Get-CimInstance -ClassName SoftwareLicensingProduct -Namespace root/CIMV2 | ForEach-Object -Process {[PSCustomObject]@{Name = $_.Name; Description = $_.Description; "Grace Period" =  [System.Math]::Round($_.GracePeriodRemaining/1440)}}
+Get-CimInstance -ClassName SoftwareLicensingProduct -Namespace root/CIMV2 | ForEach-Object -Process {[PSCustomObject]@{"Grace Period" =  [System.Math]::Round($_.GracePeriodRemaining/1440)}}
